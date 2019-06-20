597d54be782c7a0129922afb62ec57444b21180a
Lance Owen Merrill <lanceomerrill@gmail.com>
2019-06-19 18:37:48 -0700

    added the number of weeks

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ var x = setInterval(function() {
   var distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
+  var weeks = Math.floor(distance / (1000 * 60 * 60 * 24)/7);
   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -24,7 +25,7 @@ var x = setInterval(function() {
   // Display the result in the element with id="demo"
   document.getElementById("counter").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
-
+  document.getElementById('weeks').innerHTML = weeks + 'weeks'
   // If the count down is finished, write some text 
   if (distance < 0) {
     clearInterval(x);
